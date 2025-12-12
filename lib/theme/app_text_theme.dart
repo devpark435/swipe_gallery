20225eb5c021f10347b303487eb9010c733dd07e
devpark435 <devpark435@gmail.com>
2025-12-12 18:10:07 +0900

    style: baselabelmedium 추가

--- lib/theme/app_text_theme.dart
+++ lib/theme/app_text_theme.dart
@@ -35,6 +35,12 @@ class AppTextTheme {
     height: 1.3,
   );
 
+  static const _baseLabelMedium = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+    height: 1.3,
+  );
+
   // 하위 호환성을 위해 유지 (삭제해도 되지만 기존 코드 수정을 최소화하기 위해)
   // 하지만 색상은 Theme에서 덮어씌워져야 함.
   static const displayLarge = _baseDisplayLarge;
@@ -42,6 +48,7 @@ class AppTextTheme {
   static const bodyLarge = _baseBodyLarge;
   static const bodyMedium = _baseBodyMedium;
   static const labelLarge = _baseLabelLarge;
+  static const labelMedium = _baseLabelMedium;
 
   static TextTheme getTheme(AppColorTheme colors) {
     return TextTheme(
@@ -50,6 +57,7 @@ class AppTextTheme {
       bodyLarge: _baseBodyLarge.copyWith(color: colors.textPrimary),
       bodyMedium: _baseBodyMedium.copyWith(color: colors.textSecondary),
       labelLarge: _baseLabelLarge.copyWith(color: colors.surface),
+      labelMedium: _baseLabelMedium.copyWith(color: colors.textSecondary),
     );
   }
 }
